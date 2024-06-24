406c8e8497e8fcffa90bd0669143acbb0a46fc0c
pedroth <pedrotiago92@gmail.com>
2024-06-24 18:00:18 +0100

    Add previous object cache (fail)

---
+++ src/RayCache.js
@@ -0,0 +1,12 @@
+export default function RayCache() {
+    const ans = {};
+    ans.cache = {};
+    ans.set = (ray, value) => {
+
+        return ans;
+    }
+    ans.get = ray => {
+
+    }
+    return ans;
+}
